869bef82b2bfa3ac50116428baacaa8bcef049d7
Merijn <99054535@mydavinci.nl>
2022-01-24 12:12:48 +0100

    I made a number + number thing, they listen to the multiplier, however they don't want to work with the button, have to figure that on out

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
 const app = Vue.createApp({
     data() {
         return {
-            image: './images/davinci_logo.png'
+            image: './images/davinci_logo.png',
+            first_number: '',
+            second_number: '',
+            answer: ''
         }
     },
-methods: {
-    currentDate() {
-        const current = new Date();
-        const date = `${current.getDate()}/${current.getMonth()+1}`;
-        return date;
-      }
+    methods: {
+        currentDate() {
+            const current = new Date();
+            const date = `${current.getDate()}/${current.getMonth()+1}`;
+            return date;
+        },
+        calculate(first_number, second_number) {
+            answer = first_number + second_number;
+            return answer;
+        }
     }
 })
